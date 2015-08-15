ac80a119bd9b9ab91646d51ea3af2d6360d6cd1a
FalconPilot <falconpilot@MacBook-Pro-de-FalconPilot.local>
2015-08-15 19:19:08 +0200

    initial commit

---
+++ brainfuck_#C/incs/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+# define MAIN_H
+# define MEMORY	30000
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+
+typedef struct	s_cursor
+{
+	int			mem[MEMORY];
+	int			pos;
+	int			loop;
+}				t_cursor;
+
+int				r_strcmp(char *str, char *mod);
+void			d_char(char c, int fd);
+void			d_string(char *str, int fd);
+
+#endif
---
+++ brainfuck_#C/srcs/r_strcmp.c
@@ -0,0 +1,14 @@
+int		r_strcmp(char *str, char *mod)
+{
+	int		i;
+	int		ret;
+
+	ret = 0;
+	for (i = 0; str[i] && mod[i]; i++)
+		ret += str[i] - mod[i];
+	for (; str[i] && mod[i] == '\0'; i++)
+		ret += str[i];
+	for (; mod[i] && str[i] == '\0'; i++)
+		ret -= mod[i];
+	return (ret);
+}
