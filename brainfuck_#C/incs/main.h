c7b7b6f09e4a8d2f69208a3b1d71bbf0be914af0
FalconPilot <falconpilot@MacBook-Pro-de-FalconPilot.local>
2015-08-15 19:30:19 +0200

    Fix indentation

--- brainfuck_#C/incs/main.h
+++ brainfuck_#C/incs/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 # define MAIN_H
-# define MEMORY	30000
+# define MEMORY 30000
 # include <fcntl.h>
 # include <sys/types.h>
 # include <sys/uio.h>
@@ -8,13 +8,13 @@
 
 typedef struct	s_cursor
 {
-	int			mem[MEMORY];
-	int			pos;
-	int			loop;
-}				t_cursor;
+    int         mem[MEMORY];
+    int         pos;
+    int         loop;
+}               t_cursor;
 
-int				r_strcmp(char *str, char *mod);
-void			d_char(char c, int fd);
-void			d_string(char *str, int fd);
+int             r_strcmp(char *str, char *mod);
+void            d_char(char c, int fd);
+void            d_string(char *str, int fd);
 
 #endif
--- brainfuck_#C/main.c
+++ brainfuck_#C/main.c
@@ -4,84 +4,83 @@
 
 #include <main.h>
 
-void	d_errors(int code)
+void        d_errors(int code)
 {
-	switch (code)
-	{
-		case (1) : d_string("#Err[00] : Wrong argument count (2 required)\n", 2); break;
-		case (2) : d_string("#Err[01] : Incorrect call (./brainfuck filename/instructions)\n", 2);
-	}
+    switch (code)
+    {
+        case (1) : d_string("#Err[00] : Wrong argument count (2 required)\n", 2); break;
+        case (2) : d_string("#Err[01] : Incorrect call (./brainfuck filename/instructions)\n", 2);
+    }
 }
 
-t_cursor	cursor_update(t_cursor cursor, char op)
+t_cursor    cursor_update(t_cursor cursor, char op)
 {
-	cursor.loop = 0;
-
-	switch (op)
-	{
-		case ('>') : cursor.pos++; break;
-		case ('<') : cursor.pos--; break;
-		case ('+') : cursor.mem[cursor.pos]++; break;
-		case ('-') : cursor.mem[cursor.pos]--; break;
-		case ('.') : d_char((char)cursor.mem[cursor.pos], 1); break;
-		case ('[') : cursor.loop = 1;
-		case (']') : cursor.loop = 2;
-	}
-	return (cursor);
+    cursor.loop = 0;
+    switch (op)
+    {
+        case ('>') : cursor.pos++; break;
+        case ('<') : cursor.pos--; break;
+        case ('+') : cursor.mem[cursor.pos]++; break;
+        case ('-') : cursor.mem[cursor.pos]--; break;
+        case ('.') : d_char((char)cursor.mem[cursor.pos], 1); break;
+        case ('[') : cursor.loop = 1;
+        case (']') : cursor.loop = 2;
+    }
+    return (cursor);
 }
 
-void	instructions(char *inst)
+void        instructions(char *inst)
 {
-	int			i;
-	t_cursor	cursor;
+    int         i;
+    t_cursor    cursor;
 
-	cursor.pos = 0;
-	for (i = 0; i <= MEMORY; i++)
-		cursor.mem[i] = '\0';
-	for (i = 0; inst[i]; i++)
-	{
-		cursor = cursor_update(cursor, inst[i]);
-		while (cursor.loop == 1 && inst[i] != ']' && cursor.mem[cursor.pos] == '\0')
-			i++;
-		while (cursor.loop == 2 && inst[i] != '[' && cursor.mem[cursor.pos])
-			i--;
-		if (inst[i] == ',')
-		{
-			i++;
-			if (inst[i] == 92)
-				i++;
-			cursor.mem[cursor.pos] = inst[i];
-		}
-	}
-	d_string("\n", 1);
+    cursor.pos = 0;
+    for (i = 0; i <= MEMORY; i++)
+        cursor.mem[i] = '\0';
+    for (i = 0; inst[i]; i++)
+    {
+        cursor = cursor_update(cursor, inst[i]);
+        while (cursor.loop == 1 && inst[i] != ']' && cursor.mem[cursor.pos] == '\0')
+            i++;
+        while (cursor.loop == 2 && inst[i] != '[' && cursor.mem[cursor.pos])
+            i--;
+        if (inst[i] == ',')
+        {
+            i++;
+            if (inst[i] == 92)
+                i++;
+            cursor.mem[cursor.pos] = inst[i];
+        }
+    }
+    d_string("\n", 1);
 }
 
-void	open_file(int fd)
+void        open_file(int fd)
 {
-	int		i;
-	char	buf;
-	char	inst[MEMORY];
+    int     i;
+    char    buf;
+    char    inst[MEMORY];
 
-	for (i = 0; read(fd, &buf, 1); i++)
-		inst[i] = buf;
-	inst[i] = '\0';
-	instructions(inst);
-	close(fd);
+    for (i = 0; read(fd, &buf, 1); i++)
+        inst[i] = buf;
+    inst[i] = '\0';
+    instructions(inst);
+    close(fd);
 }
 
-int		main(int argc, char **argv)
+int         main(int argc, char **argv)
 {
-	int		fd;
+    int     fd;
 
-	if (argc != 2)
-	{
-		d_errors(1);
-		return (1);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd != -1)
-		open_file(fd);
-	else
-		instructions(argv[1]);
-	return (0);
+    if (argc != 2)
+    {
+        d_errors(1);
+        return (1);
+    }
+    fd = open(argv[1], O_RDONLY);
+    if (fd != -1)
+        open_file(fd);
+    else
+        instructions(argv[1]);
+    return (0);
 }
