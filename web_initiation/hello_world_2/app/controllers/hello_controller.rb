470bb0ef8ef450d294b185402f4300935caa8b17
FalconPilot <fpilot.github@gmail.com>
2016-03-21 15:42:21 +0100

    corrected overall exercise

--- web_initiation/hello_world_2/app/controllers/hello_controller.rb
+++ web_initiation/hello_world_2/app/controllers/hello_controller.rb
@@ -1,4 +1,9 @@
 class HelloController < ApplicationController
   def index
+    if params[:name] && params[:name] != ""
+      @name = params[:name]
+    else
+      @name = "world"
+    end
   end
 end
