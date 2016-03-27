40d91b5429cf3689410363d21711bd6be15e618c
falconpilot <falconpilot@hotmail.fr>
2016-03-27 23:07:59 +0200

    improved display

--- web_initiation/json_show/app/controllers/main_controller.rb
+++ web_initiation/json_show/app/controllers/main_controller.rb
@@ -2,6 +2,6 @@
 class MainController < ApplicationController
   def index
     file = File.read("#{Rails.root}/app/assets/javascripts/products.json")
-    @file = JSON.parse(file)
+    @disp = JSON.parse(file)
   end
 end
