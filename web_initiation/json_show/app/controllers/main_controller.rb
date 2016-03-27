f95ee47f50d432c1df90f1faf781ae45a3f2a45d
FalconPilot <fpilot.github@gmail.com>
2016-03-27 23:00:19 +0200

    made the application actually functional

--- web_initiation/json_show/app/controllers/main_controller.rb
+++ web_initiation/json_show/app/controllers/main_controller.rb
@@ -1,17 +1,7 @@
 #encoding: utf-8
 class MainController < ApplicationController
-  helper_method :jshow
   def index
-  end
-
-  def jshow
-    @file = File.read("#{Rails.root}/app/assets/javascripts/products.json")
-    json_hash = JSON.parse(@file)
-    buffer = ""
-    json_hash.each do |elem|
-      buffer += "#{elem.to_s.gsub(/[^0-9A-Za-z]/, '')}#{content_tag(:br)}".html_safe
-    end
-    #buffer = buffer.gsub(/[^0-9A-Za-z]/, '')
-    render :html => buffer
+    file = File.read("#{Rails.root}/app/assets/javascripts/products.json")
+    @file = JSON.parse(file)
   end
 end
