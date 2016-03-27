3b03f5569f22c197e6d54ee95b5199f6d9d78907
falconpilot <falconpilot@hotmail.fr>
2016-03-27 23:00:19 +0200

    made the application actually functional

--- web_initiation/json_show/config/application.rb
+++ web_initiation/json_show/config/application.rb
@@ -22,6 +22,5 @@ module JsonShow
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.encoding : "utf-8"
   end
 end
