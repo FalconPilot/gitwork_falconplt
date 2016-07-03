a7e8195f225610ccdb8d3c5a38f60a3d50f2ffdb
FalconPilot <fpilot.github@gmail.com>
2016-07-03 03:13:50 +0200

    fixed updating/sorting

--- elixir/web-phoenix/todoList/web/controllers/page_controller.ex
+++ elixir/web-phoenix/todoList/web/controllers/page_controller.ex
@@ -6,7 +6,7 @@ defmodule TodoList.PageController do
   def create(conn, %{"task" => task_params}) do
     buffer = Task.changeset(%Task{}, task_params)
     if (buffer.valid?) do
-      Repo.insert(buffer)
+      Repo.insert buffer
     end
     conn
     |> redirect(to: "/")
@@ -15,7 +15,7 @@ defmodule TodoList.PageController do
   # Switch status
   def switch(conn, %{"id" => id}) do
     task = Repo.get!(Task, id)
-    task = Ecto.Changeset.change task, status: task.status
+    task = Ecto.Changeset.change task, status: !task.status
     Repo.update task
     conn
     |> redirect(to: "/")
@@ -27,10 +27,28 @@ defmodule TodoList.PageController do
     |> redirect(to: "/")
   end
 
+  # Sort map
+  def list_sort(list) do
+    list_sort(list, [], [])
+  end
+
+  def list_sort([h|t], acc1, acc2) do
+    if (h.status) do
+      acc1 = [h|acc1]
+    else
+      acc2 = [h|acc2]
+    end
+    list_sort(t, acc1, acc2)
+  end
+
+  def list_sort([], acc1, acc2) do
+    acc2 ++ acc1
+  end
+
   # Index
   def index(conn, _params) do
     changeset = Task.changeset(%Task{})
-    tasklist = Repo.all(Task)
+    tasklist = list_sort(Repo.all(Task))
     conn
     |> assign(:changeset, changeset)
     |> assign(:tasklist, tasklist)
