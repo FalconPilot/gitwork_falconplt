31a74e38f84bfee4f86066f69b0537215618a5a6
FalconPilot <fpilot.github@gmail.com>
2016-07-03 03:23:00 +0200

    optimized sorting with query

--- elixir/web-phoenix/todoList/web/controllers/page_controller.ex
+++ elixir/web-phoenix/todoList/web/controllers/page_controller.ex
@@ -21,34 +21,11 @@ defmodule TodoList.PageController do
     |> redirect(to: "/")
   end
 
-  # Destroy entry
-  def destroy(conn, _params) do
-    conn
-    |> redirect(to: "/")
-  end
-
-  # Sort map
-  def list_sort(list) do
-    list_sort(list, [], [])
-  end
-
-  def list_sort([h|t], acc1, acc2) do
-    if (h.status) do
-      acc1 = [h|acc1]
-    else
-      acc2 = [h|acc2]
-    end
-    list_sort(t, acc1, acc2)
-  end
-
-  def list_sort([], acc1, acc2) do
-    acc2 ++ acc1
-  end
-
   # Index
   def index(conn, _params) do
     changeset = Task.changeset(%Task{})
-    tasklist = list_sort(Repo.all(Task))
+    query = from t in Task, order_by: t.status
+    tasklist = Repo.all(query)
     conn
     |> assign(:changeset, changeset)
     |> assign(:tasklist, tasklist)
